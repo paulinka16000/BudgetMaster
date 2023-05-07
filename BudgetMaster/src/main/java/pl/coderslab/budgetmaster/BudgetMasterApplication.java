bcad346d2ae14f25de27059e44f5d0b9179d593c
paulinka16000 <karnas.paulina@gmail.com>
2023-05-07 10:33:56 +0200

    first commit

---
+++ BudgetMaster/src/main/java/pl/coderslab/budgetmaster/BudgetMasterApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.budgetmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetMasterApplication.class, args);
+	}
+
+}
---
+++ BudgetMaster/src/test/java/pl/coderslab/budgetmaster/BudgetMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.budgetmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
